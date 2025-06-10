ab060b3c039d436649d8594547d2c958895a4608
Sean Reynolds <reynolds.sean01@gmail.com>
2025-06-09 22:20:24 -0700

    added dark mode/light mode functionality

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const navLinks = document.getElementById("nav-links");
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
